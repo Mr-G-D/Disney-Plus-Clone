e76ffd052dbf876dd9a077a9c9917f71c237e410
Mr-G-D <daredinesh00@gmail.com>
2021-05-19 22:59:24 +0530

    details page bug solved

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -11,6 +11,37 @@ function Detail() {
             alt=""
           />
         </Background>
+        <ImageTitle>
+          <img
+            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78"
+            alt=""
+          />
+        </ImageTitle>
+        <Controls>
+          <PlayButton>
+            <img src="/images/play-icon-black.png" alt="" /> <span>PLAY</span>
+          </PlayButton>
+          <TrailerButton>
+            <img src="/images/play-icon-white.png" alt="" />{" "}
+            <span>TRAILER</span>
+          </TrailerButton>
+          <AddButton>
+            <span>+</span>
+          </AddButton>
+          <GroupButton>
+            <img src="/images/group-icon.png" alt="" />
+          </GroupButton>
+        </Controls>
+        <Subtitle>
+          Disney+ Clone - ReactJS Challenge (Learn Redux, React JS, Styled
+          Components, and Firebase) | Day 1
+        </Subtitle>
+        <Description>
+          Their Firebase config file is no longer available in the linked
+          folder. You may have to set up your own Firebase database and source
+          content yourself. The images and videos are in the linked folder
+          though.
+        </Description>
       </Container>
     </div>
   );
@@ -30,9 +61,88 @@ const Background = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
+  z-index: -1;
+  opacity: 0.8;
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
   }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  width: 35vw;
+  min-width: 200px;
+  margin-top: 60px;
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  display: flex;
+  margin-right: 22px;
+  align-items: center;
+  height: 56px;
+  background-color: rgb(249, 249, 249);
+  border: none;
+  padding: 0px 24px;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+
+const AddButton = styled.button`
+  width: 44px;
+  margin-right: 16px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  border: 2px solid white;
+  cursor: pointer;
+  background: rgba(0, 0, 0, 0.6);
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const GroupButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+
+const Subtitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-heifht: 20px;
+  margin-top: 26px;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  max-width: 760px;
 `;
