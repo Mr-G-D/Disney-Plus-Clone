0022318267ee962ee7cbbadb2fddf5737b30e127
Mr-G-D <daredinesh00@gmail.com>
2021-06-03 19:05:12 +0530

    new disney section #5

--- src/components/New.js
+++ src/components/New.js
@@ -1,17 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { selectNew } from "../features/movie/movieSlice";
+import { useSelector } from "react-redux";
 
 function New() {
+  const movies = useSelector(selectNew);
   return (
     <Container>
       <h4>New to Disney+</h4>
       <Content>
-        <Wrap>
-          <Link to="/">
-            <img src="" alt="" />
-          </Link>
-        </Wrap>
+        {movies &&
+          movies.map((movie, key) => (
+            <Wrap key={key}>
+              {movie.id}
+              <Link to={"/detail/" + movie.id}>
+                <img src={movie.cardImg} alt={movie.title} />
+              </Link>
+            </Wrap>
+          ))}
       </Content>
     </Container>
   );
@@ -35,31 +42,30 @@ const Content = styled.div`
 `;
 
 const Wrap = styled.div`
-  padding-top: 56.25px;
+  padding-top: 56.25%;
   border-radius: 10px;
-  box-shadow: rgb(0 0 0/ 69%) 0px 26px 30px -10px,
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   cursor: pointer;
   overflow: hidden;
   position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
-
-  img{
+  img {
     inset: 0px;
     display: block;
     height: 100%;
     object-fit: cover;
     opacity: 1;
-    postion: absolutel
+    position: absolute;
     transition: opacity 500ms ease-in-out 0s;
     width: 100%;
     z-index: 1;
     top: 0;
   }
-
-  &:hover{
-    box-shadow: rgb(0 0 0/ 80%) 0px 40px 58px -16px, rgb(0 0 0/72%) 0px 30px 22px -10px;
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
   }
